b4a84fec76318d1343ef614e3f74117d62996520
Yuko Robinson <ggboyuko@gmail.com>
2020-12-17 17:00:49 +0900

    Dec 17th

--- portfolio.js
+++ portfolio.js
@@ -17,4 +17,30 @@ $('.tab-nav a').on('click', function(){
     
 });
 
+$(window).on('scroll', function(){
+    let summary = $('.summary_love').offset().top;
+    let footer_fadein = $('.footer_text').offset().top;
+
+    let wh = $(window).height();
+
+    let now_position = $(window).scrollTop();
+
+    if (summary <= wh + now_position){
+        // 見せる
+        $('.summary_love').removeClass('hide');
+    }　else　{
+        // 隠しておく
+        $('.summary_love').addClass('hide');
+    }
+
+    if (footer_fadein <= wh + now_position){
+        // 見せる
+        $('.footer_fade').removeClass('hide');
+    }　else　{
+        // 隠しておく
+        $('.footer_fade').addClass('hide');
+    }
+
+});
+
 });
